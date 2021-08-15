ad06cfede8391b9aad102e6a7fae45372e34bfb8
sarthak.makhija <sarthak.makhija@gmail.com>
2021-08-15 16:04:39 +0530

    Sarthak | Renames valueByKey to offsetByKey in InMemoryKeyValueTable

--- kv/appendOnly/InMemoryKeyValueTable.go
+++ kv/appendOnly/InMemoryKeyValueTable.go
@@ -1,24 +1,24 @@
 package appendOnly
 
 type InMemoryKeyValueTable struct {
-	valueByKey  map[string]Offset
+	offsetByKey map[string]Offset
 	keyValueLog *KeyValueLog
 }
 
 func NewInMemoryKeyValueTable(keyValueLog *KeyValueLog) InMemoryKeyValueTable {
 	return InMemoryKeyValueTable{
-		valueByKey:  make(map[string]Offset),
+		offsetByKey: make(map[string]Offset),
 		keyValueLog: keyValueLog,
 	}
 }
 
 func (table InMemoryKeyValueTable) Put(key string, value string) {
 	startingOffset := table.keyValueLog.Put(KeyValuePair{Key: key, Value: value})
-	table.valueByKey[key] = startingOffset
+	table.offsetByKey[key] = startingOffset
 }
 
 func (table InMemoryKeyValueTable) Get(key string) string {
-	offset, exists := table.valueByKey[key]
+	offset, exists := table.offsetByKey[key]
 	if exists {
 		return table.keyValueLog.GetAtStartingOffset(offset).Value
 	}
