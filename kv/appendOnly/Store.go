8eb90ea927e104e923525818653bb86444473135
sarthak.makhija <sarthak.makhija@gmail.com>
2021-08-16 12:31:26 +0530

    Sarthak | Store now acquires lock before populating in-memory table

--- kv/appendOnly/Store.go
+++ kv/appendOnly/Store.go
@@ -2,19 +2,26 @@ package appendOnly
 
 type Store struct {
 	inMemoryKeyValueTable *InMemoryKeyValueOffsetTable
+	fileLock              FileLock
 }
 
 func Open(fileName string) Store {
+	acquireLockOrFail := func() FileLock {
+		fileLock := AcquireExclusiveLock(fileName)
+		if fileLock.Err != nil {
+			panic(fileLock.Err)
+		}
+		return fileLock
+	}
+
 	keyValueLog := NewKeyValueLog(fileName)
+	fileLock := acquireLockOrFail()
 	inMemoryKeyValueTable := createOrLoadInMemoryKeyValueOffsetTable(keyValueLog)
 
 	return Store{
 		inMemoryKeyValueTable: &inMemoryKeyValueTable,
+		fileLock:              fileLock,
 	}
-}
-
-func (store Store) Close() {
-	store.inMemoryKeyValueTable.Close()
 }
 
 func (store Store) Put(key []byte, value []byte) {
@@ -23,6 +30,16 @@ func (store Store) Put(key []byte, value []byte) {
 
 func (store Store) Get(key []byte) []byte {
 	return store.inMemoryKeyValueTable.Get(key)
+}
+
+func (store Store) Close() {
+	releaseLockOrFail := func(fileLock FileLock) {
+		if err := fileLock.Release(); err != nil {
+			panic(err)
+		}
+	}
+	store.inMemoryKeyValueTable.Close()
+	releaseLockOrFail(store.fileLock)
 }
 
 func createOrLoadInMemoryKeyValueOffsetTable(keyValueLog KeyValueLog) InMemoryKeyValueOffsetTable {
